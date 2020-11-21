9d8924058fea8b31a953a5af76e25b6a7aae7539
田志会-Chris <tzh.goon@gmail.com>
2020-11-21 15:35:50 +0800

    feat: promise

--- routes/users.js
+++ routes/users.js
@@ -4,28 +4,42 @@ const router = express.Router()
 
 router.get('/user/:id', function (req, res, next) {
   const id = req.params.id
-  users.getUserById(id).then(res.json).catch(next)
+  users
+    .getUserById(id)
+    .then(e => res.json(e))
+    .catch(next)
 })
 
 router.post('/user', function (req, res, next) {
   const user = req.body
-  users.createUser(user).then(res.json).catch(next)
+  users
+    .createUser(user)
+    .then(e => res.json(e))
+    .catch(next)
 })
 
 router.put('/user/:id', function (req, res, next) {
   const id = req.params.id
   const data = req.body
-  users.updateUser(id, data).then(res.json).catch(next)
+  users
+    .updateUser(id, data)
+    .then(e => res.json(e))
+    .catch(next)
 })
 
 router.delete('/user/:id', function (req, res, next) {
   const id = req.params.id
-  users.deleteUser(id).then(res.json).catch(next)
+  users
+    .deleteUser(id)
+    .then(e => res.json(e))
+    .catch(next)
 })
 
 router.get('/users/all', function (req, res, next) {
-  console.log('all???')
-  users.getAllUsers().then(res.json).catch(next)
+  users
+    .getAllUsers()
+    .then(e => res.json(e))
+    .catch(next)
 })
 
 module.exports = router
