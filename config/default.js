6dfb86abc5743f22d5d983c5ce51236d4aa72fe0
田志会-Chris <tzh.goon@gmail.com>
2020-11-29 19:57:44 +0800

    feat: mongodb

--- config/default.js
+++ config/default.js
@@ -1,7 +1,6 @@
 module.exports = {
   PORT: parseInt(process.env.PORT, 10) || 8001,
   SWAGGER_HOST: 'localhost:8002',
-  MONGODB_URL: 'mongodb://127.0.0.1:27017/kanban_dev',
   JWT_SECRET: 'hao123',
   JWT_EXPIRES: 30 * 24 * 60 * 60,
   WECHAT_APP_ID: 'wx7ba7096ea6de67c8',
--- config/production.js
+++ config/production.js
@@ -1,1 +1,3 @@
-module.exports = {}
+module.exports = {
+  MONGODB_URL: 'mongodb://superuser:qwer1234@39.99.175.151:27017/kanban_dev'
+}
